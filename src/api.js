d2c0ec9ad77b2d4454b54b30ccf1789708913907
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-01-18 12:28:45 +0100

    Created a call api function

--- src/api.js
+++ src/api.js
@@ -3,6 +3,25 @@ class WarwickJS {
     this.api_key = api_key;
     this.base_uri = `https://${region}.api.riotgames.com/lol`;
   }
+
+  callApi(url) {
+    if (!this.api_key) return Promise.reject("No API key has been given");
+    return new Promise((resolve, reject) => {
+      if (!url) return reject("Bad request");
+      axios
+        .get(url, {
+          params: {
+            api_key: this.api_key,
+          },
+        })
+        .then((response) => {
+          return resolve(response.data);
+        })
+        .catch((error) => {
+          return reject(error);
+        });
+    });
+  }
 }
 
 module.exports = WarwickJS;
