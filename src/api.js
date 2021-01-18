a54a89dc913ffa06e3c1a48beebd3e5d6c008abd
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-01-18 12:42:52 +0100

    Added some documentation

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
 const axios = require("axios");
-
+/**
+ * @param {string} api_key - A Riot Games API key
+ * @param {string} region - A league of legends servers' region
+ */
 class WarwickJS {
   constructor(api_key, region) {
     this.api_key = api_key;
     this.base_uri = `https://${region}.api.riotgames.com/lol`;
   }
 
+  /**
+   * Call the API with a given url
+   * @param {string} url - The url to request
+   */
   callApi(url) {
     if (!this.api_key) return Promise.reject("No API key has been given");
     return new Promise((resolve, reject) => {
@@ -25,6 +32,10 @@ class WarwickJS {
     });
   }
 
+  /**
+   * Get id by summoners name
+   * @param {string} name - The name of a league of legends summoner
+   */
   getSummonerIdByName(name) {
     if (!name) {
       return Promise.reject(new Error("No summoner has been specified"));
@@ -40,6 +51,10 @@ class WarwickJS {
     }
   }
 
+  /**
+   * Get current game played by summoner
+   * @param {string} id - The id of a League of legends summoner
+   */
   getActiveGame(id) {
     if (!id) {
       return Promise.reject(new Error("No summoner id has been specified"));
@@ -54,7 +69,9 @@ class WarwickJS {
       });
     }
   }
-
+  /**
+   * Returns the current status of the league of legends servers
+   */
   getStatus() {
     return new Promise((resolve, reject) => {
       let url = `${this.base_uri}/status/v4/platform-data`;
